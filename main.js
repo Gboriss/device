3c8fd21dc2bc3e12243e19c09f55a9087460c158
gboriss <galinaborissenko@gmail.com>
2019-08-28 17:41:03 +0300

    main.scss

---
+++ main.js
@@ -0,0 +1,46 @@
+import './main.scss'
+
+// body {
+// 	.center {
+// 		width: 100%;
+// 		max-width: 1400px;
+// 		margin: 0 auto;
+// 		padding: 0 20px;
+// 		box-sizing: border-box;
+// 	}
+// }
+
+// .search-navigation {
+// 	input {
+// 		&:focus {
+// 			opacity: 1;
+// 			border-bottom: 2px solid #F00;
+// 			outline: none;
+// 		}
+// 	}
+// }
+
+// .search-navigation {
+
+// 	&::before {
+// 			content: "";
+			
+// 			position: absolute;
+// 			top: 12px;
+// 			left: 3px;
+			
+// 			width: 16px;
+// 			height: 16px;
+			
+// 			background-image: url("../device/device/svg/search.svg");
+// 			background-repeat: no-repeat;
+// 			background-position: 0 0;
+// 		}
+// }
+
+// .link {
+// 	&-home,
+// 	&-articles,
+// 	&-about, {	
+// 	}
+// }
